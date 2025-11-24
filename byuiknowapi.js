fc2d226a9a556fda5ef1ce2e04bb293d53daafb0
marcosdavis <macrosdavy@protonmail.com>
2025-11-24 09:03:13 -0700

    started to create api for database

---
+++ byuiknowapi.js
@@ -0,0 +1,2 @@
+// Create an api and sub parts for the different tables in the database. 
+// Also create different functions that can be called in other JS scripts that called without writing the commands everytime.
