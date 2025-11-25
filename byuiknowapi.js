b03370bd864ce7a5d1dc1a81c01259d55b670e97
marcosdavis <macrosdavy@protonmail.com>
2025-11-25 11:43:29 -0700

    Revert "Restored original index.html file and updated various other files"
    This reverts commit 8d9ce1ff84e41272fc46fd5a4d8eb6481eb13ab0.

--- byuiknowapi.js
+++ byuiknowapi.js
@@ -1,15 +1,2 @@
 // Create an api and sub parts for the different tables in the database. 
 // Also create different functions that can be called in other JS scripts that called without writing the commands everytime.
-
-function access() {
-    fetch("api/access.php")
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .then(err => console.log(err));
-}
-
-const searchBtn = document.getElementById("search-btn");
-
-searchBtn.addEventListener("click", async () => {
-    access();
-})
