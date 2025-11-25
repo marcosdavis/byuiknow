34f1ebaee3014d4dec1785cbb30967fd54b5ef6c
marcosdavis <macrosdavy@protonmail.com>
2025-11-25 15:19:38 -0700

    started image carousel on index.html

---
+++ HTML files/JS files/image-carousel.js
@@ -0,0 +1,17 @@
+document.addEventListener("back-btn", e => {
+    e.preventDefault();
+    back();
+})
+
+document.addEventListener("next-btn", e => {
+    e.preventDefault();
+    next();
+})
+
+function back() {
+    
+}
+
+function next() {
+
+}
